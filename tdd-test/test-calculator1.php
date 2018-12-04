56bcff8f92532d71a3697c0efdf41776304b24ff
Thom Harrington <thomas.harrington@seattlecentral.edu>
2018-12-04 22:14:37 +0000

    Add test files

---
+++ tdd-test/test-calculator1.php
@@ -0,0 +1,10 @@
+<?php
+//test-calculator1.php
+
+include 'Calculator.php';
+
+$myCalc = new Calculator();
+
+echo $myCalc->add(1,2);
+
+?>
---
+++ tdd-test/test-calculator2.php
@@ -0,0 +1,16 @@
+<?php
+//test-calculator2.php
+require_once 'simpletest/autorun.php';
+include 'Calculator.php';
+
+class TestOfCalculator extends UnitTestCase {
+
+    function testAdd(){
+        $myCalc = new Calculator();
+        $this->assertEqual($myCalc->add(1,2),3,"1 + 2");
+        $this->assertEqual($myCalc->add(1,2,1),4,"3 params");    
+    }
+
+}
+
+?>
